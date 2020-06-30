0845397de98863633e5ddaf8157ced4827bbc560
Haoyu <haoyuumb@gmail.com>
2020-06-29 23:52:53 -0400

    15

--- lecture_15/src/edu/umb/cs210/bst/RedBlackBST.java
+++ lecture_15/src/edu/umb/cs210/bst/RedBlackBST.java
@@ -679,14 +679,16 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        RedBlackBST<String, Integer> st = new RedBlackBST<String, Integer>();
-        for (int i = 0; !StdIn.isEmpty(); i++) {
-            String key = StdIn.readString();
-            st.put(key, i);
-        }
-        StdOut.println();
-        for (String s : st.keys())
-            StdOut.println(s + " " + st.get(s));
-        StdOut.println();
+
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+//        RedBlackBST<String, Integer> st = new RedBlackBST<String, Integer>();
+//        for (int i = 0; !StdIn.isEmpty(); i++) {
+//            String key = StdIn.readString();
+//            st.put(key, i);
+//        }
+//        StdOut.println();
+//        for (String s : st.keys())
+//            StdOut.println(s + " " + st.get(s));
+//        StdOut.println();
     }
 }
