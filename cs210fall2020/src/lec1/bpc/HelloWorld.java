00b733315f654e825960749afff5a5839408583b
Haoyu <haoyuumb@gmail.com>
2020-09-09 17:53:38 -0400

    lec1

--- cs210fall2020/src/lec1/bpc/HelloWorld.java
+++ cs210fall2020/src/lec1/bpc/HelloWorld.java
@@ -1,3 +1,15 @@
+/*
+	
+	-classpath or -cp:  means the path to the source code
+
+	-d(destnation) : means the path for storing the output java class files
+
+	Compilation: javac -d out  -cp libraries/algs4.jar src/lec1/bpc/HelloWorld.java
+	Excecition: java -cp libraries/algs4.jar:out lec1.bpc.HelloWorld
+
+
+*/
+
 
 
 
@@ -7,7 +19,10 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class HelloWorld {
     public static void main(String[] args) {
+    	//System library is within the stardard
+    	//
         System.out.println("Hello World!");
+        // The StdOut is a third-part libary that needs to import specifically
         StdOut.print("Hello World!\n");
     }
 }
