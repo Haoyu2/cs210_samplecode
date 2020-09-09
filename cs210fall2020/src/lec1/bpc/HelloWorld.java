0fef1e49db145a0e20ac7df6f4334b30f2ce7ffc
Haoyu <haoyuumb@gmail.com>
2020-09-09 15:58:38 -0400

    fall2020

---
+++ cs210fall2020/src/lec1/bpc/HelloWorld.java
@@ -0,0 +1,10 @@
+package lec1.bpc;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        StdOut.print("Hello World!\n");
+    }
+}
